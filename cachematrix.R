2970bc7ee4e745cd882c654343ab729670aa1719
Julien Grenier <julien.grenier@nuance.com>
2014-06-09 22:41:21 -0400

    Implement the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Defines two functions to implement caching for matrix inversion.
+##
 
-## Write a short comment describing this function
+## Given a matrix 'x', create a cacheable matrix. Pass the returned object to 'cacheSolve' to get the inverse.
+## The return object is a list of four(4) functions :
+##  get() -> Return the matrix 'x'
+##  set(y) -> Update 'x' to 'y' and clear the cache
+##  getinverse() -> Return the cached inverse of 'x'
+##  setinverse(i) -> Set the inverse of 'x' to 'y'. This function shall not be call outside of 'cacheSolve'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) inverse <<- i
+  getinverse <- function() inverse
+  list(get=get, set=set, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Takes a cacheable matrix created with 'makeCacheMatrix'. Return the inverse of the matrix 'x' either from the cache or after using 'solve'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)){
+    message("getting result from cache")
+    return(inverse)
+  }
+  real_matrix <- x$get()
+  inverse <- solve(real_matrix, ...)
+  x$setinverse(inverse)
+  return(inverse)
 }
